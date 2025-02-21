b40a0d85f1db0592db8d8891c88c664fed3e074f
unknown <orionu360@gmail.com>
2025-02-21 11:48:21 +0100

    code implementation

---
+++ Main.java
@@ -0,0 +1,72 @@
+
+interface BuildingFeatures {
+    void setDimension(float length, float breadth);
+    void setColor(String color);
+    void setLocation(String location);
+    void displayBuildingProperties();
+}
+
+class Building implements BuildingFeatures {
+
+    String color;
+    String location;
+    Float area;
+    boolean isStoryBuilding = false;
+
+    class StoreyBuilding {
+
+        int storeys;
+
+        StoreyBuilding(int storeys) {
+            if (storeys <= 1) throw new Error("Storey building must have a minimum of 2 floor levels");
+            this.storeys = storeys;
+            Building.this.isStoryBuilding = true;
+        }
+
+        void displayBuildingProperties() {
+            System.out.println("The buiding properties:");
+            System.out.println("Color:" + Building.this.color);
+            System.out.println("Area:" + Building.this.area);
+            System.out.println("Location:" + Building.this.location);
+            System.out.println("Levels:" + this.storeys);
+        }
+
+    }
+
+    public void setDimension (float l, float b) {
+        this.area = l * b;
+    }
+
+    public void setColor (String color) {
+        this.color = color;
+    }
+
+    public void setLocation (String location) {
+        this.location = location;
+    }
+
+    public void displayBuildingProperties() {
+        System.out.println("The buiding properties:");
+        System.out.println("Color: " + this.color);
+        System.out.println("Area: " + this.area);
+        System.out.println("Location: " + this.location);
+        System.out.println("Storey Building: " + Building.this.isStoryBuilding);
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+        //create an instance of building
+        Building house = new Building();
+
+        Building.StoreyBuilding storeyBuilding = house.new StoreyBuilding(100);
+        house.setColor("orangered");
+        house.setDimension(100, 50);
+        house.setLocation("Ikeja, Lagos");
+
+        storeyBuilding.displayBuildingProperties();
+
+    }
+}
